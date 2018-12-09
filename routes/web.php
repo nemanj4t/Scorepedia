6f02216811c6ea22c690bb893b9240e50e32d6e0
vtmr <vitomir.aka.vita@gmail.com>
2018-12-09 22:25:07 +0100

    Dodate rute u /routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,39 @@ Route::get('/', function () {
     //dd(Cypher::run("MATCH (n) RETURN n"));
     return view('welcome');
 });
+
+// TeamController
+Route::get('/teams', "TeamController@index");          // prikaz svih timova
+Route::get('/teams/{team}', "TeamController@show");    // prikaz konkretnog tima
+Route::get('/teams/create', "TeamController@create");  // pogled za kreiranje tima
+Route::post('teams', "TeamController@store");          // usnimanje kreiranog tima
+
+// PlayerController
+Route::get('/players', "PlayerController@index");          // prikaz svih igraca
+Route::get('/players/{player}', "PlayerController@show");  // prikaz konkretnog igraca
+Route::get('players/create', "PlayerController@create");   // pogled za kreiranje igraca
+Route::post('/players', "PlayerController@store");         // usnimanje kreiranog igraca
+
+// CoachController
+Route::get('/coaches', "CoachController@index");             // prikaz svih trenera
+Route::get('/coaches/{coach}', "CoachController@show");      // prikaz konkretnog trenera
+Route::get('/coaches/create', "CoachController@create");     // kreiranje novog trenera
+Route::post('/coaches', "CoachController@store");            // usnimanje novog trenra
+
+// MatchesController
+// Ukoliko nam mecevi nisu na home page-u
+Route::get('/matches', "MatchController@index");            // prikaz liste meceva (kesirani ???)
+Route::get('/matches/{match}', "MatchController@show");     // prikaz konkretnog meca
+Route::get('/matches/create', "MatchController@create");    // pogled za kreiranje meca
+Route::post('/matches', "MatchController@store");           // usnimanje kreiranog meca
+
+// StatisticController
+Route::get('/statistics', "StatisticController@index");          // prikazuje za svaku od par statistika po 5 igraca npr.
+Route::get('/statistics/points', "StatisticsController@points"); // prikazuje detaljnije statistiku za poene npr. sa vise igraca
+Route::get('/statistics/rebounds', "StatisticsController@rebounds"); // isto kao iznad
+// ...
+
+// CommentController
+// Komentari su u okviru meca
+Route::post('/matches/{match}/comments', "CommentController@store");   // usnimanje novog komentara (mozda ne treba kontroler)
+
