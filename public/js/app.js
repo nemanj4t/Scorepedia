e65da51c651e8320dad51603d47507cafdb4f9b2
nemanj4t <nemanja.turanjanin@elfak.rs>
2018-12-15 03:14:21 +0100

    msg commit

--- public/js/app.js
+++ public/js/app.js
@@ -47593,7 +47593,7 @@ var staticRenderFns = [
             _vm._v(" "),
             _c("div", { staticClass: "card-body" }, [
               _vm._v(
-                "\r\n                        I'm an example component.\r\n                    "
+                "\n                        I'm an example component.\n                    "
               )
             ])
           ])
