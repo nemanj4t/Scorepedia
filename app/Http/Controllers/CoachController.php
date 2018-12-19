59cfdf26184c51f0bb9111249c0b061830cf4554
steffanturanjanin <stefan@turanjanin.net>
2018-12-19 17:29:01 +0100

    Coach show.view

--- app/Http/Controllers/CoachController.php
+++ app/Http/Controllers/CoachController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Ahsan\Neo4j\Facade\Cypher;
 
@@ -47,6 +48,41 @@ class CoachController extends Controller
     public function show($id)
     {
         //
+        $coach = Cypher::run("MATCH (c:Coach) WHERE ID(c) = $id RETURN c")->getRecords()[0]->getPropertiesOfNode();
+
+        //tim koji trenira
+        $coachResult = Cypher::run("MATCH (t:Team)-[r:TEAM_COACH]-(c:Coach) WHERE ID(c) = $id RETURN t, r
+                                    ORDER BY r.until DESC");
+        $coached_teams = [];
+        $current_team = '';
+
+        foreach ($coachResult->getRecords() as $record) {
+            // Vraca vrednosti za tim za koji igrac igra
+            $team = $record->nodeValue('t');
+            $team_props = $team->values();
+            $team_id = ["id" => $team->identity()];
+
+            // Vraca vrednosti za relaciju TEAM_COACH
+            $relationship = $record->relationShipValue('r');
+            $relationship_props = $relationship->values();
+            $relationship_id = ["id" => $relationship->identity()];
+
+
+            // Spaja kljuceve i propertije
+            $team = array_merge($team_props, $team_id);
+            $coach_team = array_merge($relationship_props, $relationship_id);
+
+            if (Carbon::parse($relationship_props['coached_until'])->gt(Carbon::now()))
+                $current_team = $team;
+
+                    // Niz koji sadrzi relaciju i tim
+            $coaches = ['coach_team' => $coach_team, 'team' => $team];
+
+            array_push($coached_teams, $coaches);
+        }
+
+
+        return view('coaches.show', compact('coach', 'coached_teams', 'current_team'));
     }
 
 
---
+++ resources/views/coaches/show.blade.php
@@ -0,0 +1,153 @@
+@extends('layouts.app')
+
+@section('content')
+
+    {{--Style--}}
+    <style type="text/css">
+
+        .image-container {
+            position: relative;
+        }
+
+        .image {
+            opacity: 1;
+            display: block;
+            width: 100%;
+            height: auto;
+            transition: .5s ease;
+            backface-visibility: hidden;
+        }
+
+        .middle {
+            transition: .5s ease;
+            opacity: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            text-align: center;
+        }
+
+        .image-container:hover .image {
+            opacity: 0.3;
+        }
+
+        .image-container:hover .middle {
+            opacity: 1;
+        }    </style>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-9">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title mb-4">
+                            <div class="d-flex justify-content-start">
+                                <div class="image-container">
+                                    <img src="{{ $coach['image'] }}" id="image" style="width: 150px; height: 150px" class="img-thumbnail" />
+                                </div>
+                                <div class="userData ml-3">
+                                    <h2 class="d-block" style="font-size: 1.5rem; font-weight: bold"><a href="javascript:void(0);">{{ $coach['name'] }}</a></h2>
+                                    <label style="font-weight:bold;">Bio</label>
+                                    <p>{{ $coach['bio'] }}</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <ul class="nav nav-tabs mb-4" id="myTab" role="tablist">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" id="basicInfo-tab" data-toggle="tab" href="#basicInfo" role="tab" aria-controls="basicInfo" aria-selected="true">Basic Info</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="teams-tab" data-toggle="tab" href="#teams" role="tab" aria-controls="teams" aria-selected="false">Teams</a>
+                                    </li>
+                                </ul>
+                                <div class="tab-content ml-1" id="myTabContent">
+                                    <div class="tab-pane fade show active" id="basicInfo" role="tabpanel" aria-labelledby="basicInfo-tab">
+                                        <div class="row">
+                                            <div class="col-sm-3 col-md-2 col-5">
+                                                <label style="font-weight:bold;">Name</label>
+                                            </div>
+                                            <div class="col-md-8 col-6">
+                                                {{ $coach['name'] }}
+                                            </div>
+                                        </div>
+                                        <hr />
+
+                                        <div class="row">
+                                            <div class="col-sm-3 col-md-2 col-5">
+                                                <label style="font-weight:bold;">Team</label>
+                                            </div>
+                                            <div class="col-md-8 col-6">
+                                                @if($current_team == '')
+                                                    <p>No proffessional engagement currently</p>
+                                                    @else
+                                                    <p><a href="/teams/{{$current_team['id']}}">{{$current_team['name']}}</a></p>
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <hr />
+
+                                        <div class="row">
+                                            <div class="col-sm-3 col-md-2 col-5">
+                                                <label style="font-weight:bold;">City</label>
+                                            </div>
+                                            <div class="col-md-8 col-6">
+                                                {{ $coach['city'] }}
+                                            </div>
+                                        </div>
+
+                                        <hr />
+                                    </div>
+                                    {{--Teams tab--}}
+                                    <div class="tab-pane fade" id="teams" role="tabpanel" aria-labelledby="teams-tab">
+                                        <div class="container" style="margin-top:20px;">
+                                            <div class="row">
+                                                <div id="user" class="col-md-12" >
+                                                    <div class="panel panel-primary panel-table animated slideInDown">
+                                                        <div class="panel-body">
+                                                            <div class="tab-content">
+                                                                <div role="tabpanel" class="tab-pane active" id="list">
+                                                                    <table class="table table-striped table-bordered table-list">
+                                                                        <thead>
+                                                                        <tr>
+                                                                            <th class="avatar">Logo</th>
+                                                                            <th>Team</th>
+                                                                            <th>Coached since</th>
+                                                                            <th>Coached until</th>
+                                                                        </tr>
+                                                                        </thead>
+                                                                        <tbody>
+                                                                        @foreach ($coached_teams as $team)
+                                                                            <tr class="ok">
+                                                                                <td class="avatar"><img id="img" src={{$team['team']['image']}}></td>
+                                                                                <td><a href="/teams/{{$team['team']['id']}}">{{$team['team']['name']}}</a></td>
+                                                                                <td>{{$team['coach_team']['coached_since']}}</td>
+                                                                                <td>{{$team['coach_team']['coached_until']}}</td>
+                                                                            </tr>
+                                                                        @endforeach
+                                                                        </tbody>
+                                                                    </table>
+                                                                </div><!-- END id="list" -->
+
+                                                            </div><!-- END tab-content -->
+                                                        </div>
+                                                    </div><!--END panel-table-->
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{--Sidebar za preporuke igraca--}}
+        </div>
+    </div>
+
+@endsection
