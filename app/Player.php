ec081f6ded5c4d19902c09fb589ef21e13413051
vtmr <vitomir.aka.vita@gmail.com>
2018-12-30 17:31:49 +0100

    Dodavanje globalne statistike prilikom kreiranja igraca

--- app/Player.php
+++ app/Player.php
@@ -3,6 +3,9 @@
 namespace App;
 use Ahsan\Neo4j\Facade\Cypher;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use App\Player_Team;
+use App\PlayerStatistic;
 
 class Player extends Model
 {
@@ -24,6 +27,35 @@ class Player extends Model
         $player = array_merge($playerProps, ['id' => $id]);
 
         return $player;
+    }
+
+    public static function savePlayer(Request $request) {
+        // Ovaj upit moze da vrati id na kraju
+        Cypher::run("CREATE (:Player {name: '$request[name]', height: '$request[height]',
+            weight: '$request[weight]', city: '$request[city]', bio: '$request[bio]', image: '$request[image]'})");
+
+        $result = Cypher::run("MATCH (n:Player) WHERE n.name = '$request[name]' return ID(n)")->getRecords();
+        $player_id = $result[0]->values()[0];
+        $keys_array = ["team_name", "player_number", "player_position", "player_since", "player_until"];
+
+        $count = 0;
+        // Dok ne nadje prvi input za vezu tim_igrac koji je prazan
+        while($request[$keys_array[0] . '_' . $count] != null)
+        {
+            $rel = array();
+            $rel['player_id'] = $player_id;
+            foreach($keys_array as $key)
+            {
+                $rel[$key] = $request[$key . "_" . $count];
+            }
+            $plays = new Player_Team($rel);
+            $plays->save();
+
+            $count++;
+        }
+
+        // Dodavanje globalne statistike za ovog igraca u redis
+        PlayerStatistic::saveGlobalStats($player_id);
     }
 
 }
--- app/PlayerStatistic.php
+++ app/PlayerStatistic.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Database\Eloquent\Model;
 
 class PlayerStatistic extends Model
@@ -12,4 +13,42 @@ class PlayerStatistic extends Model
     public $steals;
     public $assists;
     public $fouls;
+
+    public static function saveGlobalStats($id) {
+        Redis::zadd(
+            "players:points",
+            0,
+            "player:{$id}"
+        );
+
+        Redis::zadd(
+            "players:blocks",
+            0,
+            "player:{$id}"
+        );
+
+        Redis::zadd(
+            "players:rebounds",
+            0,
+            "player:{$id}"
+        );
+
+        Redis::zadd(
+            "players:steals",
+            0,
+            "player:{$id}"
+        );
+
+        Redis::zadd(
+            "players:assists",
+            0,
+            "player:{$id}"
+        );
+
+        Redis::zadd(
+            "players:fouls",
+            0,
+            "player:{$id}"
+        );
+    }
 }
