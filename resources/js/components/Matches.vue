fc79d751214f81ca3337f350ee0b75d2a505779d
nemanj4t <nemanja.turanjanin@elfak.rs>
2019-01-14 10:24:34 +0100

    'finishing'

--- resources/js/components/Matches.vue
+++ resources/js/components/Matches.vue
@@ -1,13 +1,13 @@
 <template>
-    
+
      <div>
         <!-- Live Matches -->
         <!-- Bindovanje da proverim -->
         <div class="container card mt-4 mb-4" v-if="liveMatches.length > 0">
             <div class="card-body">
                 <h3>Live</h3>
-                <div class="row justify-content-between" v-for="match in liveMatches">
-                        <div class="col-md-5 m-4" style="border: dashed 4px; padding: 20px">
+                <div class="row justify-content-between" >
+                        <div class="col-md-5 m-4" style="border: dashed 4px; padding: 20px" v-for="match in liveMatches">
                             <div class="row">
                                 <div class="col-md-4">
                                     <img style="max-width: 100%;" v-bind:src="match.team_match.home.image">
@@ -33,13 +33,13 @@
                 </div>
             </div>
         </div>
-        
+
         <!-- Upcoming Matches -->
         <div class="container card mt-4 mb-4" v-if="upcomingMatches.length > 0">
             <div class="card-body">
                 <h3>Upcoming</h3>
-                <div class="row justify-content-between" v-for="match in upcomingMatches">
-                        <div class="col-md-5 m-4" style="border: dashed 1px; padding: 20px">
+                <div class="row justify-content-between">
+                        <div class="col-md-5 m-4" style="border: dashed 1px; padding: 20px" v-for="match in upcomingMatches">
                             <div class="row">
                                 <div class="col-md-4">
                                     <img style="max-width: 100%;" v-bind:src="match.team_match.home.image">
@@ -70,8 +70,8 @@
         <div class="container card mt-4 mb-4" v-if="finishedMatches.length > 0">
             <div class="card-body">
                 <h3>Finished</h3>
-                <div class="row justify-content-between" v-for="match in finishedMatches">
-                        <div class="col-md-5 m-4" style="border: dashed 1px; padding: 20px">
+                <div class="row justify-content-between" >
+                        <div class="col-md-5 m-4" style="border: dashed 1px; padding: 20px" v-for="match in finishedMatches">
                             <div class="row">
                                 <div class="col-md-4">
                                     <img style="max-width: 100%;" v-bind:src="match.team_match.home.image">
@@ -96,12 +96,12 @@
                         </div>
                 </div>
             </div>
-        </div>      
-    </div> 
+        </div>
+    </div>
 </template>
 
 <script>
- 
+
 
     export default {
         name: "matches",
@@ -114,7 +114,7 @@
         },
 
         methods: {
-        
+
         },
 
         mounted() {
@@ -123,13 +123,13 @@
                    this.liveMatches = response.data.liveMatches;
                    this.finishedMatches = response.data.finishedMatches;
                    this.upcomingMatches = response.data.upcomingMatches;
-                }); 
+                });
         },
 
         sockets : {
             liveMatches (data) {
                 let newData = JSON.parse(data);
-                
+
                 let match = this.liveMatches.find(m => {
                     return m.id === Number(newData.matchId);
                 });
